b44634053524a26150b6005ed159c46767d28a1c
liad-ahrak <liadahrak@gmail.com>
2023-04-25 18:38:06 +0300

    pagination

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d00f7c5d3da7f5d6"; }
+/******/ 		__webpack_require__.h = function() { return "71cac18ed9ba1ace"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
