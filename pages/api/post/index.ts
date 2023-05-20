9c6767d6b4e8e4ebb34ef4e9010f2ec708e7b4bd
lihibenb <lihibenb@post.bgu.ac.il>
2023-05-20 21:12:03 +0300

    cloudinary

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -2,12 +2,11 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
 
 
-
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { title, content, video, session, email} = req.body;
+  const { title, content, session, email} = req.body;
 
   
   if (session) {
